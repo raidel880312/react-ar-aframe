583b1f15878fb7324737e6425c1b5a2c1e0a6c28
Raidel Gonzalez Naranjo <raidel.gonzalez@endava.com>
2020-08-07 09:29:12 -0300

    Creating app

---
+++ build/precache-manifest.eea2303b7f865ecbb062ad762720f021.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f1f0609c63f4f11a9e824f64b85b429",
+    "url": "/arjs-gestures/react-ar-aframe/public/index.html"
+  },
+  {
+    "revision": "d7a8cc95603c40849a1d",
+    "url": "/arjs-gestures/react-ar-aframe/public/static/css/main.b04462b0.chunk.css"
+  },
+  {
+    "revision": "dcdf5b094d8c70e30ea7",
+    "url": "/arjs-gestures/react-ar-aframe/public/static/js/2.e29543af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/arjs-gestures/react-ar-aframe/public/static/js/2.e29543af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a8cc95603c40849a1d",
+    "url": "/arjs-gestures/react-ar-aframe/public/static/js/main.f4bf498e.chunk.js"
+  },
+  {
+    "revision": "3453c570fa8149267abc",
+    "url": "/arjs-gestures/react-ar-aframe/public/static/js/runtime-main.75a19f8e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/arjs-gestures/react-ar-aframe/public/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="arjs-loader">
+        <div>Loading, please wait...</div>
+      </div>
+      <a-scene
+          vr-mode-ui="enabled: false;"
+          renderer="logarithmicDepthBuffer: true;"
+          embedded
+          arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
+      >
+
+        <a-nft
+            markerhandler
+            type="nft"
+            url="https://arjs-cors-proxy.herokuapp.com/https://raw.githubusercontent.com/wclaro/arjs-gestures/master/images/cutcsa"
+        >
+        </a-nft>
+        <a-entity camera></a-entity>
+      </a-scene>
+    </div>
+  );
+}
+
+export default App;

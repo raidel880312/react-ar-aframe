a460bebf5c415a968bd51b6a4d778b9aed104bc0
Raidel Gonzalez Naranjo <raidel.gonzalez@endava.com>
2020-08-07 09:33:50 -0300

    Updates

---
+++ precache-manifest.7eac5bab90dc2405881f782efe09af35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "911e5c826d56cecd74e042b822535f36",
+    "url": "/react-ar-aframe/index.html"
+  },
+  {
+    "revision": "ea99f032aa3393a53c6f",
+    "url": "/react-ar-aframe/static/css/main.b04462b0.chunk.css"
+  },
+  {
+    "revision": "dcdf5b094d8c70e30ea7",
+    "url": "/react-ar-aframe/static/js/2.e29543af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-ar-aframe/static/js/2.e29543af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea99f032aa3393a53c6f",
+    "url": "/react-ar-aframe/static/js/main.179257d9.chunk.js"
+  },
+  {
+    "revision": "c6e3ceb486a1a1b3c1e6",
+    "url": "/react-ar-aframe/static/js/runtime-main.8032eb4f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-ar-aframe/static/media/logo.5d5d9eef.svg"
+  }
+]);
